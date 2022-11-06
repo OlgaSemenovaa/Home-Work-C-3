393bf56c42a7c8331faff587d5074dffb87957e3
Olga Semenova <olya.meoow@yandex.ru>
2022-11-07 02:52:50 +0300

    Task23 done

---
+++ Task21/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и 
+// находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+Console.WriteLine("Введите координаты точки A: ");
+Console.Write("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z: ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B: ");
+Console.Write("X1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y1: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z1: ");
+int z3 = Convert.ToInt32(Console.ReadLine());
+
+double distance = Distance(x, x1, y, y1, z, z1);
+Console.WriteLine(Math.Round(distance, 2, MidpointRounding.ToZero));
+
+double Distance(int a1, int a2, int b1, int b2, int c1, int c2);
+{
+    double dist = Math.Sqrt((a1 - a2)*(a1 - a2)+(b1 - b2)*(b1 - b2)+(c1 - c2)*(c1 - c2));
+    return dist;
+}
---
+++ Task23/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число (N) и 
+// выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+void Table (int n)
+{
+    if (n > 0)
+    {
+        int index = 1;
+        while (index <= n)
+        {
+            Console.WriteLine($"{index} | {index*index*index}");
+            index++;
+        }
+    }
+    else Console.WriteLine("Некорректное Число");
+}
+
+Console.WriteLine("Введине натуральное число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Table(n);
