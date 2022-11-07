9867d0142f1d5da17a2b53d81af5e990d7ec6a0a
Olga Semenova <olya.meoow@yandex.ru>
2022-11-07 03:42:01 +0300

    Task21 done

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -5,27 +5,28 @@
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
 
-Console.WriteLine("Введите координаты точки A: ");
-Console.Write("X: ");
-int x = Convert.ToInt32(Console.ReadLine());
-Console.Write("Y: ");
-int y = Convert.ToInt32(Console.ReadLine());
-Console.Write("Z: ");
-int z = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координаты точки B: ");
+Console.WriteLine("Введите координаты точки A: ");
 Console.Write("X1: ");
 int x1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Y1: ");
-int y2 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Z1: ");
-int z3 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
 
-double distance = Distance(x, x1, y, y1, z, z1);
+Console.WriteLine("Введите координаты точки B: ");
+Console.Write("X2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double distance = Distance(x1, x2, y1, y2, z1, z2);
 Console.WriteLine(Math.Round(distance, 2, MidpointRounding.ToZero));
 
-double Distance(int a1, int a2, int b1, int b2, int c1, int c2);
+double Distance(int a1, int a2, int b1, int b2, int c1, int c2)
 {
-    double dist = Math.Sqrt((a1 - a2)*(a1 - a2)+(b1 - b2)*(b1 - b2)+(c1 - c2)*(c1 - c2));
+    double dist = Math.Sqrt((a1 - a2) * (a1 - a2) + (b1 - b2) * (b1 - b2) + (c1 - c2) * (c1 - c2));
     return dist;
 }
